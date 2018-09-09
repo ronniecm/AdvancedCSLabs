a2fab090b98b1721b2034088ac54cb6c7c58cc3c
ronniecm <ronniecm@ronnies-mbp.fios-router.home>
2018-09-09 00:00:54 -0400

    RationalNum and Polynomial Completed

--- src/FirstTerm/ArrayBasedPoly.java
+++ src/FirstTerm/ArrayBasedPoly.java
@@ -1,13 +1,30 @@
+/*
+ * Name: Ronnie Mohapatra
+ * Lab: Polynomial Lab
+ * Date: September 8th, 2018
+ * What I Learned: a.) I gained a whole new perspective on how interfaces can be used. I now realize
+ * 						that they can be used kind of as an overlap between several different classes.
+ * 				   b.) You must test your program several times before finishing. Otherwise, you may not have
+ * 						the proper results.
+ */
 package FirstTerm;
 
 public class ArrayBasedPoly implements Polynomial {
 	private double[] c;
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: coefficients is copied to c
+	 */
 	public ArrayBasedPoly(double[] coefficients)
 	{
 		c = coefficients;
 	}
 	
+	/*
+	 * pre-condition: exponent > 0
+	 * post-condition: c is filled with zeros excpet for the index-based degree
+	 */
 	public ArrayBasedPoly(double coefficient, int exponent)
 	{
 		c = new double[exponent + 1];
@@ -19,6 +36,10 @@ public class ArrayBasedPoly implements Polynomial {
 		}
 	}
 	 
+	/*
+	 * pre-condition: copy is of type Polynomial
+	 * post-condition: attributes of copy are also attributes of this
+	 */
 	public ArrayBasedPoly(Polynomial copy)
 	{
 		c = new double[copy.getDegree() + 1];
@@ -28,6 +49,10 @@ public class ArrayBasedPoly implements Polynomial {
 		}
 	}
 	
+	/*
+	 * pre-condition: 0 < exponent < c.length
+	 * post-condition: c[exponent] is returned
+	 */
 	public double getCoefficient(int exponent)
 	{
 		return c[exponent];
@@ -38,21 +63,26 @@ public class ArrayBasedPoly implements Polynomial {
 		return c.length - 1;
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: p(x) is returned
+	 */
 	public double evaluate(int x)
 	{
 		double answer = 0;
 		
 		for(int i = 0; i < c.length; i++)
 		{
-			if(i == 0)
-				answer += c[0];
-			else
-				answer += this.getCoefficient(i) * Math.pow(x, i);
+			answer += this.getCoefficient(i) * Math.pow(x, i);
 		}
 		
 		return answer;
 	}
 	
+	/*
+	 * pre-condition: other is of type Polynomial
+	 * post-condition: new Polynomial is returned which represents the sum of this and other
+	 */
 	public Polynomial add(Polynomial other)
 	{
 		double[] c2;
@@ -89,6 +119,10 @@ public class ArrayBasedPoly implements Polynomial {
 		return new ArrayBasedPoly(c2);
 	}
 	
+	/*
+	 * pre-condition: other is of type Polynomial
+	 * post-condition: new Polynomial is returned which represents the difference between this and other
+	 */
 	public Polynomial subtract(Polynomial other)
 	{
 		double[] c2;
@@ -123,6 +157,10 @@ public class ArrayBasedPoly implements Polynomial {
 		return new ArrayBasedPoly(c2);
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: new Polynomial returned which represents the derivative of this
+	 */
 	public Polynomial findDerivative()
 	{
 		double[] c2 = new double[c.length - 1];
@@ -134,6 +172,10 @@ public class ArrayBasedPoly implements Polynomial {
 		return new ArrayBasedPoly(c2);
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: ArrayBasedPoly printed out with highest degree term first
+	 */
 	public String toString()
 	{
 		String result = this.getCoefficient(c.length-1) + "x^" + (c.length-1);
@@ -156,22 +198,45 @@ public class ArrayBasedPoly implements Polynomial {
 		return result;
 	}
 	
+	public static void main(String[] args) { 
+	      double [] c = {1, 0, 3, 4};
+	      double [] c1 = {-2, -5};
+	   
+	      Polynomial p1 = new ArrayBasedPoly(c);  // 4x^3 + 3x^2 + 1
+	      System.out.println("p1(x) = " + p1);
+	   
+	      Polynomial p2 = new ArrayBasedPoly(c1);   // - 5x – 2
+	      System.out.println("p2(x) = " + p2);                 // p2(x) = - 5x^1 – 2
+	   
+	      Polynomial p3 = new ArrayBasedPoly(-4, 1);  // coeff = -4, exp = 1
+	      System.out.println("p3(x) = " + p3);
+	   
+	      Polynomial p = p1.add(p2).add(p2);   // 4x^3 + 3x^2 - 10x – 3
+	      System.out.println("p(x) = " + p);       // p(x) = 4x^3 + 3x^2 - 10x^1 – 3
+	      
+	      Polynomial p4   = p.subtract(p3);   // 4x^3 + 3x^2 - 6x^1 – 3   <====
+	      System.out.println("p4(x) = " + p4);
+	   
+	   
+	      Polynomial p5   = p4.findDerivative();   // 12x^2 + 6x^1 - 6   <====
+	      System.out.println("p5(x) = " + p5);
+	      
+	      Polynomial clone = new ArrayBasedPoly(p5);
+	      System.out.println("clone(x) = " + clone);
+	   
+	      System.out.println ("p(5) = " + p5.evaluate(0));
+	      System.out.println ("p(1) = " + p5.evaluate(1));
+	   }
 	
-	
-	public static void main(String[] args)
-	{
-		double [] c = {1, 0, 3, 4};
-		double[] c2 = {3, 1, 2};
-		Polynomial p = new ArrayBasedPoly(c);
-		Polynomial p2 = new ArrayBasedPoly(c2);
-		Polynomial p3 = p.add(p2);
-		Polynomial p4 = p.subtract(p2);
-		Polynomial p5 = p.findDerivative();
-		
-		System.out.println("p(x) = " + p);
-		System.out.println("p2(x) = " + p2);
-		System.out.println("p3(x) = " + p3);
-		System.out.println("p4(x) = " + p4);
-		System.out.println("p5(x) = " + p5);
-	}	
+	/* Output
+		p1(x) = 4.0x^3 + 3.0x^2 + 1.0
+		p2(x) = -5.0x^1 - 2.0
+		p3(x) = -4.0x^1
+		p(x) = 4.0x^3 + 3.0x^2 - 10.0x^1 - 3.0
+		p4(x) = 4.0x^3 + 3.0x^2 - 6.0x^1 - 3.0
+		p5(x) = 12.0x^2 + 6.0x^1 - 6.0
+		clone(x) = 12.0x^2 + 6.0x^1 - 6.0
+		p(5) = -6.0
+		p(1) = 12.0
+	 */
 }
--- src/FirstTerm/RationalNum.java
+++ src/FirstTerm/RationalNum.java
@@ -1,21 +1,52 @@
+/*
+ * Name: Ronnie Mohapatra
+ * Lab: RationalNum
+ * Date: September 8, 2018
+ * What I Learned: a.) I learned a new and improved way to find the greatest common factor between
+ * 						two numbers using a recursive method and the mod operator.
+ * 				   b.) I also found a new way to compute the least common multiple between two numbers using
+ * 						the greatest common factor and the product of the two numbers. It is num1 * num2 / GCD(num1, num2).
+ */
 package FirstTerm;
 
 public class RationalNum {
 	private int numerator, denominator;
 	
+	/*
+	 * pre-condition: denom > 0
+	 * post-condition: a simplified rational number has constructed
+	 * 					numerator = numer and denominator = denom
+	 */
 	public RationalNum(int numer, int denom)
 	{
 		numerator = numer;
 		denominator = denom;
-		simplify();
+		simplify(); //simplifies the fraction completely
 	}
 	
+	public int getNumerator()
+	{
+		return numerator;
+	}
+	
+	public int getDenominator()
+	{
+		return denominator;
+	}
+	/*
+	 * pre-condition: none
+	 * post-condition: instance variables of this RationalNum equal the instance variables of other
+	 */
 	public RationalNum(RationalNum copy)
 	{
 		numerator = copy.numerator;
 		denominator = copy.denominator;
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: denominator = denom
+	 */
 	public void setDenom(int denom)
 	{
 		if(denom == 0)
@@ -26,19 +57,32 @@ public class RationalNum {
 		simplify();
 	}
 	
+	/*
+	 * pre-condition: numerator and denominator have a value
+	 * post-condition: numerator and denominator are divded by their greatest common factor
+	 */
 	private void simplify()
 	{
-		int gcd = findGCD(numerator, denominator);
+		int gcd = GCD(numerator, denominator);
 		numerator /= gcd;
 		denominator /= gcd;
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: new RationalNum of the sum between this and other returned
+	 */
 	public RationalNum add(RationalNum other)
 	{
-		int lcm = findLCM(this.denominator, other.denominator);
+		int lcm = findLCM(this.denominator, other.denominator); 
 		int newNumerator = (this.numerator * (lcm / this.denominator)) + (other.numerator * (lcm / other.denominator));
 		return new RationalNum(newNumerator, lcm);	
 	}
+	
+	/*
+	 * pre-condition: none
+	 * post-condition: new RationalNum of the difference between this and other returned
+	 */
 	
 	public RationalNum subtract(RationalNum other)
 	{
@@ -48,43 +92,97 @@ public class RationalNum {
 		return new RationalNum(newNumerator, lcm);
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: new RationalNum of the product between this and other returned
+	 */
 	public RationalNum multiply(RationalNum other)
 	{
 		return new RationalNum(this.numerator * other.numerator, this.denominator * other.denominator);
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: new RationalNum of the quotient between this and other returned
+	 */
 	public RationalNum divide(RationalNum other)
 	{
 		return new RationalNum(this.numerator * other.denominator, this.denominator * other.numerator);
 	}
 	
-	public boolean equals(RationalNum other)
+	/*
+	 * pre-condition: other is of type RationalNum
+	 * post-condition: returns true if numerator and denominator of this is equal to the numerator and denominator of other
+	 */
+	public boolean equals(Object other)
 	{
-		if(this.numerator == other.numerator && this.denominator == other.denominator)
+		RationalNum n = (RationalNum)other;
+		if(this.numerator == n.getNumerator() && this.denominator == n.getDenominator()) //compares numerator and denominator
 			return true;
 		else
 			return false;
 	}
 	
+	/*
+	 * pre-condition: none
+	 * post-condition: RationalNum printed out in "numerator / denominator" format
+	 */
 	public String toString()
 	{
 		return this.numerator + "/" + this.denominator;	
 	}
 	
-	private int findGCD(int num1, int num2)
+	/*
+	 * pre-condition: num >= 0 and denom > 0
+	 * post-condition: greatest common factor between num and denom is returned
+	 */
+	private int GCD(int num, int denom)
 	{
-		while(num2 > 0)
-		{
-			int temp = num2;
-			num2 = num1 % num2;
-			num1 = temp;
-		}
-		
-		return num1;
+		if(denom == 0)
+			return num;
+		return GCD(denom, num % denom); //if denom != 0, then keep rerunning method with denom and num % denom as parameters
 	}
 	
+	/*
+	 * pre-condition: num1 >= 0 and num2 > 0
+	 * post-condition: least common multiple between num1 and num2 is found
+	 */
 	private int findLCM(int num1, int num2)
 	{
-		return (num1 * num2) / findGCD(num1, num2);
+		return num1 * num2 / GCD(num1, num2); //formula for LCM: LCM = num1 * num2 / GCD(num1, num2)
 	}
+	
+	public static void main(String [] args) 
+	{
+		RationalNum r1 = new RationalNum(52, 36); 
+		System.out.println("r1 = " + r1); 
+		RationalNum r2 = new RationalNum(7, 39); 
+		System.out.println("r2 = " + r2 + "\n");
+		r2.setDenom(0);
+		System.out.println("r2 = " + r2 + "\n"); 
+		r2.setDenom(14);
+		System.out.println("r2 = " + r2 + "\n"); 
+		System.out.println("r1 + r2: " + r1.add(r2)); 
+		System.out.println("r1 - r2: " + r1.subtract(r2));
+		System.out.println("r1 * r2: " + r1.multiply(r2)); 
+		System.out.println("r1 / r2: " + r1.divide(r2)); 
+		System.out.println("r1 equals r2: " + r1.equals(r2));
+	}
+	
+	/* Output
+	 * r1 = 13/9
+	   r2 = 7/39
+
+	  	Cannot set denominator to 0
+		r2 = 7/39
+	
+		r2 = 1/2
+
+		r1 + r2: 35/18
+		r1 - r2: 17/18
+		r1 * r2: 13/18
+		r1 / r2: 26/9
+		r1 equals r2: false
+
+	 */
 }
